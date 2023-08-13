0b682c912745a4d9bc892bda8d2ae51c7c5ed258
amer1zoo <69459543+amer1zoo@users.noreply.github.com>
2023-08-13 21:29:46 +0300

    Update ProductCard.jsx

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,4 +1,5 @@
-﻿import * as React from 'react';
+import * as React from 'react';
+import { useState, useEffect } from 'react';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
@@ -10,10 +11,46 @@ import Grid from '@mui/material/Grid';
 import CreateIcon from '@mui/icons-material/Create';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { Link, useRouteMatch, Switch, Route, useParams } from 'react-router-dom';
-
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
 
 export default function ProductCard({ id, name, icon, price, details }) {
-    
+
+    const [messageError, setmessageError] = useState('Errors');
+
+    const [openTrue, setOpenTrue] = React.useState(false);
+    const [openFalse, setOpenFalse] = React.useState(false);
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpenTrue(false);
+        setOpenFalse(false);
+    };
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        const response = await fetch('http://localhost:8080/api/products/' + id, {
+            method: 'DELETE',
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json;charset=UTF-8",
+                "Authorization": 'Bearer ' + localStorage.getItem('token')
+            }
+        });
+
+        if (response.ok) {
+            setOpenTrue(true);
+            setmessageError(`Product ${name} deleted successfully`);
+
+        } else {
+            const result = await response.json();
+            setOpenFalse(true);
+            setmessageError(result.message);
+        }
+    }
 
     return (
         <Card style={{ display: "flex", flexDirection: "column" }} sx={{ minWidth: 400, m: 3, height: { xs: 150, sm: 400 }, }} >
@@ -44,16 +81,28 @@ export default function ProductCard({ id, name, icon, price, details }) {
                     </Grid>
                     <Grid item xs={6} align="right">
                         <IconButton aria-label="edit">
-                           
-                                <Link to={"/editproduct/" + id}><CreateIcon /></Link>
-                            
+
+                            <Link to={"/editproduct/" + id}><CreateIcon /></Link>
+
                         </IconButton>
                         <IconButton aria-label="remove">
-                            <Link to={"/products/" + id}><DeleteIcon /></Link>
+                            <DeleteIcon onClick={handleSubmit} />
                         </IconButton>
                     </Grid>
                 </Grid>
             </CardActions>
+
+            <Snackbar open={openTrue} autoHideDuration={2000} onClose={handleClose} anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
+                <Alert variant="filled" onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+                    {messageError}
+                </Alert>
+            </Snackbar>
+
+            <Snackbar open={openFalse} autoHideDuration={2000} onClose={handleClose} anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
+                <Alert variant="filled" onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+                    There are some errors! {messageError}
+                </Alert>
+            </Snackbar>
         </Card>
 
     );
